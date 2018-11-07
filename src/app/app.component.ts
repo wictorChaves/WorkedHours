384a02836823a4895e037fef536f7d7826578831
wictor <wictor@hotmail.com.br>
2018-11-07 00:32:40 -0200

    Melhorias no layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'startActive', component: StartActiveComponent }
+      { title: 'Iniciar atividade', component: StartActiveComponent }
     ];
 
   }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -14,16 +14,12 @@ export class ListPage {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
     this.items = [];
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        icon: 'alarm'
       });
     }
   }
