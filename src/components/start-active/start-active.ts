ad4510a5d39763119068c6f5e99e339166f79eb0
wictor <wictor@hotmail.com.br>
2018-11-19 13:52:58 -0200

    Setando os horários

--- src/components/start-active/start-active.ts
+++ src/components/start-active/start-active.ts
@@ -15,6 +15,8 @@ import * as firebase from 'Firebase';
 })
 export class StartActiveComponent {
 
+  id;
+  total = DatetimeHelper.second2time(0);
   Job = {};
   items = [];
   labelBtnStart = "Iniciar Atividade";
@@ -22,12 +24,14 @@ export class StartActiveComponent {
   ref = firebase.database().ref('job/');
 
   constructor(public navCtrl: NavController, private navParams: NavParams) {
-
-    let newInfo = firebase.database().ref('job/' + navParams.get('id'));
+    this.id = navParams.get('id');
+    let newInfo = firebase.database().ref('job/' + this.id);
     newInfo.on('value', resp => {
       this.Job = resp.val();
       if (!this.Job.hasOwnProperty('worked')) {
         this.Job['worked'] = [];
+      }else{
+        this.updateListTime();
       }
     });
   }
@@ -44,7 +48,27 @@ export class StartActiveComponent {
     let worked: any[] = this.Job['worked'];
     worked.push(DatetimeHelper.getTimeStamp());
     this.Job['worked'] = worked;
+    this.updateListTime();
     this.changeLabel();
+  }
+
+  updateJob() {
+    firebase.database().ref('job/' + this.id).update(this.Job);
+  }
+
+  updateListTime() {
+    if (this.Job['worked'].length % 2 == 0) {
+      var i = 2;
+      this.items = [];
+      var countTotal = 0;
+      for (i = 2; i <= this.Job['worked'].length; i += 2) {
+        let seconds = this.Job['worked'][i - 1] - this.Job['worked'][i - 2];
+        this.items.push(DatetimeHelper.second2time(seconds));
+        countTotal = countTotal + seconds;
+      }
+      this.total = DatetimeHelper.second2time(countTotal);
+    }
+    this.updateJob();
   }
 
 }
--- src/helper/DatetimeHelper.ts
+++ src/helper/DatetimeHelper.ts
@@ -2,4 +2,11 @@ export class DatetimeHelper {
     static getTimeStamp() {
         return Math.round((new Date()).getTime() / 1000);
     }
+
+    static second2time(seconds) {
+        var date = new Date(null);
+        date.setSeconds(seconds);
+        var timeString = date.toISOString().substr(11, 8);
+        return timeString;
+    }
 }
